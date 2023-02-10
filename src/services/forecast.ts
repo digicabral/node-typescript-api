43550f8c98cbd553067b26e8e2754f875c81fc6d
Rodrigo Cabral <digicabral@gmail.com>
2023-02-10 15:56:16 -0300

    Grouping forecasts by time

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -15,12 +15,17 @@ export interface Beach{
     user: string;
 }
 
+export interface TimeForecast {
+    time: string;
+    forecast: BeachForecast[];
+}
+
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint{}
 
 export class Forecast {
     constructor(protected stormGlass = new StormGlass()){}
     
-    public async processForecastForBeaches(beaches: Beach[]): Promise<BeachForecast[]>{
+    public async processForecastForBeaches(beaches: Beach[]): Promise<TimeForecast[]>{
 
         const pointsWithCorrectSources: BeachForecast[] = [];
         for(const beach of beaches){
@@ -37,6 +42,23 @@ export class Forecast {
             }));
             pointsWithCorrectSources.push(...enrichedBeachData);
         }
-        return pointsWithCorrectSources;
+        return this.mapForecastBytime(pointsWithCorrectSources);
+    }
+
+    private mapForecastBytime(forecast: BeachForecast[]): TimeForecast[]{
+        const forecastByTime: TimeForecast[] = [];
+        for(const point of forecast){
+            const timePoint = forecastByTime.find((f)=> f.time == point.time)
+            if(timePoint){
+                timePoint.forecast.push(point)
+            }
+            else{
+                forecastByTime.push({
+                    time: point.time,
+                    forecast: [point]
+                })
+            }
+        }
+        return forecastByTime
     }
 }
