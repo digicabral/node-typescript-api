071ea5a7bc7632f323ac703ad9ae96fd8c66b9b4
Rodrigo Cabral <digicabral@gmail.com>
2023-02-23 15:17:08 -0300

    Added user validation errors

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,33 +1,47 @@
 import mongoose, { Document, Model } from 'mongoose';
-import { BaseModel } from '.';
 
-export interface User extends BaseModel {
-    _id?: string;
-    name: string;
-    email: string;
-    password: string;
+export interface User {
+  _id?: string;
+  name: string;
+  email: string;
+  password: string;
 }
 
-interface UserModel extends Omit<User, '_id'>, BaseModel, Document{}
+export enum CUSTOM_VALIDATION {
+  DUPLICATED = 'DUPLICATED',
+}
 
-const schema = new mongoose.Schema({
-    name: {type: String, required: true},
+interface UserModel extends Omit<User, '_id'>, Document {}
+
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
     email: {
-        type: String,
-        required: [true, 'Email is required'],
-        unique: [true, 'E-mail must be unique'],
+      type: String,
+      required: true,
     },
-    password: {type: String, required: true}
+    password: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
     },
-    {
-        toJSON: {
-          transform: (_, ret): void => {
-            ret.id = ret._id.toString();
-            delete ret._id;
-            delete ret.__v;
-          },
-        },
-      }
+  }
+);
+/**
+ * Validates the email and throws a validation error, otherwise it will throw a 500
+ */
+schema.path('email').validate(
+  async (email: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ email });
+    return !emailCount;
+  },
+  'already exists in the database.',
+  CUSTOM_VALIDATION.DUPLICATED
 );
 
 export const User: Model<UserModel> = mongoose.model('User', schema);
