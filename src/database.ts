ad50755ea004178c5bd9b412fa3830e9fcbf5d3c
Rodrigo Cabral <digicabral@gmail.com>
2023-02-28 12:30:10 -0300

    added console log

--- src/database.ts
+++ src/database.ts
@@ -4,6 +4,7 @@ import { connect as mongooseConnect, connection } from 'mongoose';
 const dbConfig: IConfig = config.get('App.database');
 
 export const connect = async (): Promise<void> => {
+  console.log(dbConfig.get('mongoUrl'));
   await mongooseConnect(dbConfig.get('mongoUrl'));
 };
 
